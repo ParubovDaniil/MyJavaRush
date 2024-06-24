c3a0e85230ce236906389237abce10749e31449e
ParubovDaniil <ParubovDaniil@mail.ru>
2024-06-24 19:10:57 +0500

    lvl5_1-2 : 6_1-3

---
+++ src/Java_Syntax_Pro/L6/lvl6_1.java
@@ -0,0 +1,19 @@
+package Java_Syntax_Pro.L6;
+
+public class lvl6_1 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
+
+
+/*
+*
+В методе main проинициализируй переменные intArray и doubleArray массивами соответствующих типов, размер которых равен 10.
+
+Требования:
+•	В методе main(String[]) проинициализируй переменную intArray согласно условию.
+•	В методе main(String[]) проинициализируй переменную doubleArray согласно условию.
+* */
---
+++ src/Java_Syntax_Pro/L6/lvl6_3.java
@@ -0,0 +1,23 @@
+package Java_Syntax_Pro.L6;
+
+public class lvl6_3 {
+    public static int[] array = new int[]{-1, 2, 3, -4, -5};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5 ; i++) {
+            
+        }
+        //напишите тут ваш код
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+}
+/*
+*
+Реализуй метод main(String[]), который меняет знак элемента массива array на противоположный, если значение этого элемента чётное.
+Требования:
+•	Реализуй метод main(String[]) согласно условию.
+* */
