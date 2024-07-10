e6d451ec1d30442fdfabd1033a5c56329da6646a
ParubovDaniil <ParubovDaniil@mail.ru>
2024-07-10 21:11:18 +0500

    lvl6_3-4(+-)

--- src/Java_Syntax_Pro/L6/lvl6_3.java
+++ src/Java_Syntax_Pro/L6/lvl6_3.java
@@ -4,9 +4,22 @@ public class lvl6_3 {
     public static int[] array = new int[]{-1, 2, 3, -4, -5};
 
     public static void main(String[] args) {
-        for (int i = 0; i < 5 ; i++) {
-            
+        if (array[0] % 2 == 0){
+            array[0] = array[0] *-1;
         }
+        if (array[1] % 2 == 0){
+            array[1] = array[1] *-1;
+        }
+        if (array[2] % 2 == 0){
+            array[2] = array[2] *-1;
+        }
+        if (array[3] % 2 == 0){
+            array[3] = array[3] *-1;
+        }
+        if (array[4] % 2 == 0){
+            array[4] = array[4] *-1;
+        }
+
         //напишите тут ваш код
         System.out.println(array[0]);
         System.out.println(array[1]);
@@ -20,4 +33,12 @@ public class lvl6_3 {
 Реализуй метод main(String[]), который меняет знак элемента массива array на противоположный, если значение этого элемента чётное.
 Требования:
 •	Реализуй метод main(String[]) согласно условию.
+* */
+
+/*
+* for (int i = 0; i < array.length; i++) {
+            if (array[i]%2==0){
+                array[i]=-array[i];
+
+            }
 * */
---
+++ src/Java_Syntax_Pro/L6/lvl6_4.java
@@ -0,0 +1,33 @@
+package Java_Syntax_Pro.L6;
+
+public class lvl6_4 {
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    public static int[] resultArray;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int dlina = firstArray.length + secondArray.length;
+        int[] resultArray = new int[dlina];
+        int a = firstArray.length;
+        for (int i = 0; i < a; i++) {
+            resultArray[i] = firstArray[i];
+            System.out.print(resultArray[i] + ", ");
+        }
+
+        int b = secondArray.length;
+        for (int i = 0; i < b; i++) {
+            resultArray[i] = secondArray[i];
+            System.out.print(resultArray[i] + ", ");
+        }
+    }
+}
+/*
+* Реализовать метод main(String[]), в котором нужно скопировать содержимое массивов firstArray и secondArray в один массив resultArray.
+Массив firstArray должен быть в начале нового массива resultArray, а secondArray — после него.
+Программа должна правильно обрабатывать массивы любой длины.
+Требования:
+•	В классе Solution должна быть публичная статическая переменная firstArray типа int[], проинициализированная массивом с 10-ю значениями.
+•	В классе Solution должна быть публичная статическая переменная secondArray типа int[], проинициализированная массивом с 10-ю значениями.
+•	В классе Solution должна быть публичная статическая переменная resultArray типа int[].
+•	Реализуй метод main(String[]) согласно условию.*/
