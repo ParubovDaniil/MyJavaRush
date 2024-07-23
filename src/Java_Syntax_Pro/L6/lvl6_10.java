ab97875b9e3bd69de4feccf600982dcd72b8abcd
ParubovDaniil <ParubovDaniil@mail.ru>
2024-07-23 23:28:28 +0500

    lvl6_10

--- src/Java_Syntax_Pro/L6/lvl6_10.java
+++ src/Java_Syntax_Pro/L6/lvl6_10.java
@@ -9,20 +9,24 @@ public class lvl6_10 {
     public static void main(String[] args) {
         //напишите тут ваш код
         result = new int[10][];
-        result[0] = new int[1];
-        result[1] = new int[2];
-        result[2] = new int[3];
-        result[3] = new int[4];
-        result[4] = new int[5];
-        result[5] = new int[6];
-        result[6] = new int[7];
-        result[7] = new int[8];
-        result[8] = new int[9];
-        result[9] = new int[10];
-        for (int i = 0; i < result.length; i++) {
-            for (int j = i+1; j < 10; j++) {
+//        result[0] = new int[1];
+//        result[1] = new int[2];
+//        result[2] = new int[3];
+//        result[3] = new int[4];
+//        result[4] = new int[5];
+//        result[5] = new int[6];
+//        result[6] = new int[7];
+//        result[7] = new int[8];
+//        result[8] = new int[9];
+//        result[9] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i+1 ; j++) {
+                result[i] = new int[i+1];
+                for (int k = 0; k < 1 ; k++) {
+                    result[i][j]=i+j;
+                }
 
-                System.out.print(result[i][j]);
+                System.out.print(result[i][j]+ " ");
             }
             System.out.println();
         }
