e61c5fee86e22975c3e715b76279b5b02d699fcb
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-20 17:32:40 +0500

    lvl10_16-17

---
+++ src/Java_Syntax_Pro/L10/lvl10_16.java
@@ -0,0 +1,23 @@
+package Java_Syntax_Pro.L10;
+/*
+String.format()
+*/
+public class lvl10_16 {
+    public static void main(String[] args) {
+        System.out.println(format("Амиго", 5000));
+    }
+
+    public static String format(String name, int salary) {
+        String phrase = "Меня зовут %s. Я буду зарабатывать $%d в месяц.";
+        return String.format(phrase, name, salary);
+    }
+}
+/*
+Сделай так, чтобы метод format(String name, int salary) возвращал строку в таком виде:
+Меня зовут <name>. Я буду зарабатывать $<salary> в месяц.
+
+Для этого используй метод String.format().
+
+Требования:
+•	Нужно, чтобы метод format(String, int) использовал метод String.format().
+•	Нужно, чтобы метод format(String, int) возвращал строку согласно условию задачи.*/
