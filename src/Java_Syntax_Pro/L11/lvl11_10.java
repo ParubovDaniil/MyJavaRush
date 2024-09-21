2b6d42e76599b523ab95f4fd3f8685033a8b44c4
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-21 15:29:04 +0500

    lvl11_10

---
+++ src/Java_Syntax_Pro/L11/lvl11_10.java
@@ -0,0 +1,45 @@
+package Java_Syntax_Pro.L11;
+import java.util.Objects;
+/*
+Два айфона
+*/
+public class lvl11_10 {}
+class Iphone {
+    private String model;
+    private String color;
+    private int price;
+
+    public Iphone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Iphone))
+            return false;
+        if (this.model== ((Iphone) obj).model && this.color == ((Iphone) obj).color && this.price == ((Iphone) obj).price){
+        return true;
+        }
+        return false;
+    }
+    //напишите тут ваш код
+
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone("X", "Black", 999);
+        Iphone iphone2 = new Iphone("X", "Black", 999);
+        System.out.println(iphone1.equals(iphone2));
+    }
+}
+/*
+*
+В методе main создаются два айфона с одинаковыми параметрами.
+В консоли выводится результат их сравнения.
+Разберись, почему сейчас результат отрицательный и сделай так, чтобы он был положительным.
+Для этого тебе нужно переопределить метод equals(Iphone), который будет учитывать все параметры.
+У двух равных объектов значения полей должны быть одинаковыми. Метод main не участвует в проверке.
+
+Требования:
+•	В классе Iphone должен быть переопределен метод public boolean equals(Object).
+•	Метод equals должен возвращать true для двух равных объектов типа Iphone и false — для разных.
+•	Метод equals должен возвращать false, если в него передали null.*/
