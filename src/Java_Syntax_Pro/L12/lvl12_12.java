ff59fbcab153496706aaf4e086d86e8de6e15f75
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 21:56:47 +0500

    lvl12_12

---
+++ src/Java_Syntax_Pro/L12/lvl12_12.java
@@ -0,0 +1,17 @@
+package Java_Syntax_Pro.L12;
+
+public class lvl12_12 {}
+class Solution12 {
+    public static void main(String[] args) {
+        String name = "Diego";
+        int age = 35;
+        System.out.println("Name : "+ name +" Age : "+ age);
+    }
+}
+/*
+"Кон", "котэ" и "нация". Как соединить эти три строки? Ты же помнишь, как соединять String'и в Java?
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+
+Требования:
+В методе main класса Solution нужно исправить ошибки: при выводе на экран должна использоваться конкатенация строк.
+*/
