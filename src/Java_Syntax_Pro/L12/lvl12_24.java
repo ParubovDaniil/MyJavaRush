62625861ea09b6c67c54a78cc044c8dbad57afa0
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 22:50:56 +0500

    lvl12_24

---
+++ src/Java_Syntax_Pro/L12/lvl12_24.java
@@ -0,0 +1,25 @@
+package Java_Syntax_Pro.L12;
+/*
+MacCoffee 3 в 1
+*/
+
+public class lvl12_24 {}
+
+/*
+public class Coffee {
+    public static class Milk {
+    }
+}
+
+class Sugar {
+}*/
+/*
+У нас есть набор классов, чтобы сделать кофе три в одном,
+но почему-то кофе не заваривается — задача не компилируется.
+Найди и исправь одну ошибку в коде.
+
+        Требования:
+        •	В файле Coffee.java должен быть класс Coffee.
+        •	В файле Coffee.java должен быть класс Sugar.
+        •	В классе Coffee должен быть public static класс Milk.
+        •	Программа должна компилироватьс*/
