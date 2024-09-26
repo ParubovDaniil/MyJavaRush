03e90a9016f4b4699fb0217a1ea35d8dd277cc03
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 21:58:10 +0500

    lvl12_13

---
+++ src/Java_Syntax_Pro/L12/lvl12_13.java
@@ -0,0 +1,18 @@
+package Java_Syntax_Pro.L12;
+
+public class lvl12_13 {}
+class Solution13 {
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected!");
+        }
+    }
+}
+/*
+Кажется, у нас есть лишняя скобка?
+Или одной не хватает?
+Разберись-ка с этой проблемой.
+
+Требования:
+В методе main класса Solution нужно исправить ошибки компиляции.
+*/
