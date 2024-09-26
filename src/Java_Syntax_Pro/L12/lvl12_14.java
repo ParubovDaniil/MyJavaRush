50f2ff7edc5739d6941eb22b84b89143c2c16d06
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 22:03:16 +0500

    lvl12_14

---
+++ src/Java_Syntax_Pro/L12/lvl12_14.java
@@ -0,0 +1,20 @@
+package Java_Syntax_Pro.L12;
+
+public class lvl12_14 {}
+class Solution14 {
+    public static void main(String[] args) {
+        int number = 1;
+        if (number < 5) {
+            System.out.println("number < 5");
+        } else {
+            System.out.println();
+        }
+    }
+}
+/*
+Кажется, в этот раз проблема снова со скобками.
+Но уже с другими. Исправь ошибки в коде, чтобы программа компилировалась и работала.
+
+Требования:
+•	В методе main класса Solution нужно исправить ошибки компиляции.
+*/
