71e6aa19ef5513dcf8efd53d5c238ea6aad97394
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 22:15:42 +0500

    lvl12_18

---
+++ src/Java_Syntax_Pro/L12/lvl12_18.java
@@ -0,0 +1,16 @@
+package Java_Syntax_Pro.L12;
+import java.util.*;
+public class lvl12_18 {}
+/*
+Импортный код
+*/
+
+ class Solution18 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
+
