b352da42e6976e2dbc85906ea3c2692b7cd3c8e4
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 21:52:59 +0500

    lvl12_10

---
+++ src/Java_Syntax_Pro/L12/lvl12_10.java
@@ -0,0 +1,17 @@
+package Java_Syntax_Pro.L12;
+
+public class lvl12_10 {
+}
+class Solution10 {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+    }
+}
+/*
+* Точки и запятые важны не только в пунктуации в обычном тексте.
+* Исправь ошибки в коде, чтобы программа компилировалась и работала.
+
+Требования:
+•	В методе main класса Solution должны быть исправлены ошибки компиляции.
+*/
