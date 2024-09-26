f06967709ef0782ad0bc72e14aaf8bf77ea1159d
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 22:27:44 +0500

    lvl12_20

---
+++ src/Java_Syntax_Pro/L12/lvl12_20.java
@@ -0,0 +1,21 @@
+package Java_Syntax_Pro.L12;
+
+import java.util.Scanner;
+/*
+Все буквы маленькие
+*/
+public class lvl12_20 {}
+class Solution20 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        System.out.println(line.toLowerCase());
+    }
+}
+/*
+Программа должна считать с консоли строку и вывести ее на экран в нижнем регистре.
+Исправь ошибку в коде, чтобы программа работала корректно.
+
+Требования:
+•	Программа должна считать с консоли строку и вывести ее на экран в нижнем регистре.
+*/
