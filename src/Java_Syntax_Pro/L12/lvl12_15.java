5f0cc29888f8b17879286bc87ca89bf541218337
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 22:07:55 +0500

    lvl12_15

---
+++ src/Java_Syntax_Pro/L12/lvl12_15.java
@@ -0,0 +1,16 @@
+package Java_Syntax_Pro.L12;
+
+/*
+Многострадальный метод main
+*/
+public class lvl12_15 {}
+class Solution15 {
+    public static void main(String... errors) {
+        System.out.println("It works!");
+    }
+}
+/*
+Сделай одно исправление в коде, чтобы программа запускалась.
+
+Требования:
+В методе main класса Solution нужно сделать одно исправление, чтобы программа заработала.*/
