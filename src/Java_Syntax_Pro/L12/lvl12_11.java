b3f79d80dda2ca2628a45c9042fb2a30c02b1187
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 21:55:44 +0500

    lvl12_11

---
+++ src/Java_Syntax_Pro/L12/lvl12_11.java
@@ -0,0 +1,9 @@
+package Java_Syntax_Pro.L12;
+
+public class lvl12_11 {}
+class Solution11 {
+    public static void main(String[] args) {
+        String string = "Amigo";
+        System.out.println(string);
+    }
+}
