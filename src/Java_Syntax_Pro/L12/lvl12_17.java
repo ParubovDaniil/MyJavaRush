8436096ce4213cc72b54a35f554ed0ef374cd3a7
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 22:13:57 +0500

    lvl12_17

---
+++ src/Java_Syntax_Pro/L12/lvl12_17.java
@@ -0,0 +1,18 @@
+package Java_Syntax_Pro.L12;
+//package com.javarush.task.pro.task11.task1117;
+/*
+Пакет нужен?
+*/
+public class lvl12_17 {}
+
+class Solution17 {
+    public static void main(String[] args) {
+        System.out.println("TestPackage");
+    }
+}
+/*
+Исправь ошибку в коде, чтобы программа компилировалась и работала.
+
+Требования:
+Программа должна компилироваться и работать.
+*/
