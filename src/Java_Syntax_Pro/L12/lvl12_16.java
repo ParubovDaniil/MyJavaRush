ba2cd497fef03cf91a569798a14c07884f1f3527
ParubovDaniil <ParubovDaniil@mail.ru>
2024-09-26 22:12:17 +0500

    lvl12_16

---
+++ src/Java_Syntax_Pro/L12/lvl12_16.java
@@ -0,0 +1,28 @@
+package Java_Syntax_Pro.L12;
+
+/*
+Правильный класс
+*/
+
+public class lvl12_16 {
+}
+/*
+    public class Solution16 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat.name);
+    }
+}
+ class Cat {
+    String name = "Tom";
+}*/
+
+/*
+*
+Исправь ошибки в коде, чтобы программа компилировалась и работала.
+
+Требования:
+Программа должна компилироваться и работать.
+В программе должен быть публичный класс Solution.
+В программе должен быть класс Ca
+* */
