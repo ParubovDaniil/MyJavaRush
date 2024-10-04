df79667fdd97c233ec1b4b51535a4b555f29b184
ParubovDaniil <ParubovDaniil@mail.ru>
2024-10-04 11:23:36 +0500

    lvl13_16

---
+++ src/Java_Syntax_Pro/L13/lvl13_16.java
@@ -0,0 +1,35 @@
+package Java_Syntax_Pro.L13;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Вперед в будущее
+*/
+public class lvl13_16 {}
+class Solution16 {
+    private int number = 54;
+    private String string = "string";
+
+    public static void main(String[] args) {
+        var integer = 22;
+        var string = "string";
+        var array = new int[5];
+        var strings = new ArrayList<String>();
+        var bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        var maxValue = Integer.valueOf(Integer.MAX_VALUE);
+    }
+}
+/*
+В классе Solution есть поля number, string и метод main с объявленными в нем переменными.
+Тебе нужно заменить объявления переменных на var, где это возможно. Порядок следования переменных не должен меняться.
+Пример:
+Scanner console = new Scanner(System.in);
+Заменить на:
+var console = new Scanner(System.in);
+
+Требования:
+•	Замени типы переменных на var, где это возможно.
+•	Не изменяй правую часть выражени
+*/
