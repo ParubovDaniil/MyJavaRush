f6a5dec565ce619f7e9c4e255bedd1fa7d6273a1
ParubovDaniil <ParubovDaniil@mail.ru>
2024-10-04 14:01:20 +0500

    lvl14_2

---
+++ src/Java_Syntax_Pro/L14/lvl14_2.java
@@ -0,0 +1,36 @@
+package Java_Syntax_Pro.L14;
+import java.util.Arrays;
+import java.util.HashSet;
+import static java.util.Arrays.asList;
+/*
+Проверка присутствия
+*/
+public class lvl14_2 {}
+class Solution2 {
+    public static HashSet<String> words = new HashSet<>(asList("Если бы меня попросили выбрать язык на замену Java я бы не выбирал".split(" ")));
+
+    public static void checkWords(String word) {
+        if (words.contains(word)){
+            System.out.println("Слово ["+word+"] есть в множестве");
+        }else {
+            System.out.println("Слова ["+word+"] нет в множестве");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkWords("JavaScript");
+        checkWords("Java");
+    }
+}
+/*В классе Solution есть метод checkWords(String), который должен проверять наличие переданного слова в множестве words.
+Если слово есть, то выводим в консоль:
+Слово [переданное слово] есть в множестве
+Если нет:
+Слова [переданное слово] нет в множестве
+Метод main не участвует в проверке.
+Пример вывода:
+Слово Java есть в множестве
+Требования:
+•	В классе Solution должно быть публичное статическое поле words типа HashSet<String>.
+•	В классе Solution должен быть публичный статический метод checkWords(String) с типом возвращаемого значения void.
+•	Метод checkWords(String) должен работать согласно условию.*/
