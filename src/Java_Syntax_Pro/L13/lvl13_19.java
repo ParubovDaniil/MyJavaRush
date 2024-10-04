d4dda434cb9e116e81682842d8e8b520b2246877
ParubovDaniil <ParubovDaniil@mail.ru>
2024-10-04 11:44:44 +0500

    lvl13_19

---
+++ src/Java_Syntax_Pro/L13/lvl13_19.java
@@ -0,0 +1,30 @@
+package Java_Syntax_Pro.L13;
+
+import java.util.ArrayList;
+
+/*
+Двойные фигурные скобки
+*/
+public class lvl13_19 {
+}
+
+class Solution19 {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>() {{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+
+    }
+}
+/*
+В методе main создается список ArrayList<String>, присваивается переменной var strings и заполняется пятью элементами.
+Нужно переписать код, не изменяя логику, используя двойные фигурные скобки.
+
+Требования:
+•	Список strings должен заполняться пятью элементами в двойных фигурных скобках.
+*/
