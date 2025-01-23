9d5141608f58a19000c043e5eb8b0518ccc1096d
ParubovDaniil <ParubovDaniil@mail.ru>
2025-01-23 14:52:01 +0500

    lvl17_10

---
+++ src/Java_Syntax_Pro/L17/lvl17_10.java
@@ -0,0 +1,38 @@
+package Java_Syntax_Pro.L17;
+
+import java.time.LocalTime;
+
+/*
+Нужно просто посчитать
+*/
+public class lvl17_10 {
+}
+
+
+class Solution10 {
+
+    public static void main(String[] args) throws InterruptedException {
+        LocalTime localTime = LocalTime.MIDNIGHT;
+        LocalTime next = amazingMethod(localTime);
+        while (next.isAfter(localTime)) {
+            System.out.println(next);
+            next = amazingMethod(next);
+            Thread.sleep(500);
+        }
+    }
+
+    static LocalTime amazingMethod(LocalTime base) {
+        if (base.getHour()==LocalTime.MIDNIGHT.getHour()) {
+            System.out.println(base);
+        }
+        return  base.plusHours(6);
+
+    }
+}
+/*
+Цикл while в методе main должен отработать ровно четыре раза, чтобы на экран вывелись 4 строки.
+Изменения можно вносить только в метод amazingMethod.
+
+Требования:
+•	Метод amazingMethod должен быть изменен так, чтобы при запуске программа вывела 4 строки.
+*/
