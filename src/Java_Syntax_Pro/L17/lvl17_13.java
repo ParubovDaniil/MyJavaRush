d95d2440f007351f570c1446f685673fd3b149b3
ParubovDaniil <ParubovDaniil@mail.ru>
2025-01-27 19:28:39 +0500

    lvl17_13

---
+++ src/Java_Syntax_Pro/L17/lvl17_13.java
@@ -0,0 +1,45 @@
+package Java_Syntax_Pro.L17;
+import java.time.Instant;
+
+import static java.time.Instant.*;
+
+/*
+Конец времен
+*/
+
+public class lvl17_13 {}
+
+
+class Solution13 {
+    private static final Instant MAX = Instant.MAX; // Добавили константу MAX
+
+
+    public static void main(String[] args) {
+        System.out.println(getMaxFromMilliseconds());
+        System.out.println(getMaxFromSeconds());
+        System.out.println(getMaxFromSecondsAndNanos());
+    }
+
+    static Instant getMaxFromMilliseconds() {
+        return Instant.ofEpochMilli(Long.MAX_VALUE);
+    }
+
+    static Instant getMaxFromSeconds() {
+        return Instant.ofEpochSecond(Instant.MAX.getEpochSecond());
+    }
+
+    static Instant getMaxFromSecondsAndNanos() {
+        return Instant.ofEpochSecond(Instant.MAX.getEpochSecond(), 999999999);
+    }
+}
+
+/*
+В классе Solution реализуй три метода:
+
+в методе getMaxFromMilliseconds верни максимальный Instant, который можно получить с помощью метода ofEpochMilli(long milliseconds);
+в методе getMaxFromSeconds верни максимальный Instant, который можно получить с помощью метода ofEpochSecond(long seconds);
+в методе getMaxFromSecondsAndNanos верни максимальный Instant, который можно получить с помощью метода ofEpochSecond(long seconds, long nanos).
+Требования:
+•	Метод getMaxFromMilliseconds нужно реализовать согласно условию.
+•	Метод getMaxFromSeconds нужно реализовать согласно условию.
+•	Метод getMaxFromSecondsAndNanos нужно реализовать согласно условию.*/
