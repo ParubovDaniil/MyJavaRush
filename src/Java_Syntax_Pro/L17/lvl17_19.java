3eeed9bcf0b1aa2ebc0bdbb3642aaf191416840f
ParubovDaniil <ParubovDaniil@mail.ru>
2025-01-29 11:52:17 +0500

    lvl17_19

---
+++ src/Java_Syntax_Pro/L17/lvl17_19.java
@@ -0,0 +1,39 @@
+package Java_Syntax_Pro.L17;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+/*
+Еще один простой шаблон
+*/
+public class lvl17_19 {}
+
+
+ class Solution19 {
+
+    static ZonedDateTime zonedDateTime = ZonedDateTime.now();
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("e d.M.yy HH:mm:ss.n VV");
+        System.out.println(zonedDateTime.format(dtf));
+    }
+}
+/*
+* В методе main выведи на экран переменную zonedDateTime в таком виде:
+4 9.3.19 06:03:07.319180500 Europe/Kiev
+
+Где:
+
+4 — номер дня недели, т.е. четверг;
+9 — день месяца;
+3 — месяц;
+19 — год;
+06 — часы;
+03 — минуты;
+07 — секунды;
+319180500 — наносекунды;
+Europe/Kiev — временная зона.
+Требования:
+•	Метод main должен вывести в консоли значение переменной zonedDateTime согласно шаблону.*/
