0543d1417f4809868dd0026d06bfabfb2d7f0706
ParubovDaniil <ParubovDaniil@mail.ru>
2025-02-06 15:46:55 +0500

    lvl20_1

---
+++ src/Java_Syntax_Pro/L20/lvl20_1.java
@@ -0,0 +1,19 @@
+/*
+package Java_Syntax_Pro.L20;
+
+import com.javarush.engine.cell.Game;
+import com.javarush.engine.cell.Color;
+public class lvl20_1 extends Game {
+    @Override
+    public void initialize() {
+
+    }
+}Чтобы запустить игру, создай класс FirstGame и унаследуй его от класса Game.
+Также переопредели метод "игрового движка" — initialize(). Попробуй запустить на выполнение класс FirstGame и посмотри, что получилось.
+Требования:
+•	Должен быть создан класс FirstGame.
+•	Класс FirstGame должен быть публичным.
+•	Класс FirstGame должен быть наследником класса Game.
+•	В классе FirstGame должен быть переопределен метод initialize() родительского класса Game.
+•	В классе FirstGame должен быть импорт класса com.javarush.engine.cell.Game
+*/
