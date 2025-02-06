ad30e920a23dcbdf8006c893454de41ee08486df
ParubovDaniil <ParubovDaniil@mail.ru>
2025-02-06 12:40:03 +0500

    lvl19_22

---
+++ src/Java_Syntax_Pro/L19/lvl19_22.java
@@ -0,0 +1,41 @@
+package Java_Syntax_Pro.L19;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+/*
+Из потока данных — в map
+*/
+public class lvl19_22 {}
+
+
+
+class Solution22 {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("JavaRush", "CodeGym", "Amigo", "Elly", "Kim", "Risha");
+
+        getMap(stringStream).forEach((s, i) -> System.out.println(s + " - " + i));
+    }
+
+    public static Map<String, Integer> getMap(Stream<String> stringStream) {
+        //напишите тут ваш код
+        return stringStream
+                .collect( Collectors.toMap(e -> e,e-> e.length()) );
+    }
+}
+/*
+Необходимо, чтобы метод getMap(Stream<String>) преобразовывал входящий поток строк в карту (map),
+ключом которой является сама строка, а значением — её длина.
+Реализуй его, используя метод collect() объекта типа Stream<String>.
+В качестве параметра передай нужный коллектор (объект типа Collector<Map<String, Integer>>).
+Такой объект можно получить, вызвав статический метод toMap() класса Collectors.
+
+Метод main() не принимает участие в тестировании.
+
+Требования:
+•	В публичном статическом методе getMap(Stream<String>) нужно вызывать статический метод toMap() класса Collectors.
+•	Метод getMap(Stream<String>) нужно реализовать согласно условию.*/
