c8272bf51c767aba99ec56fd85a54673c154cec1
ParubovDaniil <ParubovDaniil@mail.ru>
2025-02-08 10:19:54 +0500

    lvl21_8

---
+++ src/Java_Syntax_Pro/L21/lvl21_8.java
@@ -0,0 +1,38 @@
+package Java_Syntax_Pro.L21;
+
+public class lvl21_8 {}
+
+
+/*
+Скрытный инкапсулированный кот
+*/
+
+ class Solution8 {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat() {
+        }
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+    }
+}
+/*
+*
+Скрой все внутренние переменные класса Cat.
+
+Требования:
+•	Переменная name класса Cat должна быть скрыта.
+•	Переменная age класса Cat должна быть скрыта.
+•	Переменная weight класса Cat должна быть скрыта.
+•	В классе Cat должны быть 3 переменные.
+•	Все переменные класса Cat должны быть private.*/
