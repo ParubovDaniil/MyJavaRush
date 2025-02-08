6412989489942a1ea2de3294e0b23972fb465cd3
ParubovDaniil <ParubovDaniil@mail.ru>
2025-02-08 10:23:29 +0500

    lvl21_10

---
+++ src/Java_Syntax_Pro/L21/lvl21_10.java
@@ -0,0 +1,48 @@
+package Java_Syntax_Pro.L21;
+
+/*
+Неприступный кот
+*/
+
+public class lvl21_10 {}
+
+class Solution10 {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        private void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        private void setAge(int age) {
+            this.age = age;
+        }
+    }
+}
+/*
+* Скрой все внутренние переменные класса Cat, а также методы, позволяющие менять внутреннее состояние объектов класса Cat.
+
+Требования:
+•	Переменная name класса Cat должна быть скрыта.
+•	Переменная age класса Cat должна быть скрыта.
+•	Переменная weight класса Cat должна быть скрыта.
+•	В Cat должны быть private и public методы.*/
