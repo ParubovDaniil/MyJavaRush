d529c6b936e8497c5d45c25c8f2f185e05e9c389
ParubovDaniil <ParubovDaniil@mail.ru>
2025-02-08 10:33:53 +0500

    lvl21_13

---
+++ src/Java_Syntax_Pro/L21/lvl21_13.java
@@ -0,0 +1,33 @@
+package Java_Syntax_Pro.L21;
+
+public class lvl21_13 {}
+
+/*
+«Из ребра его»
+*/
+
+class Solution13 {
+    public static void main(String[] args) {
+    }
+
+    //Адам
+    public class Adam {
+
+    }
+
+    //Ева
+    public class Eve extends Adam {
+
+    }
+}
+/*
+*
+Измени два класса — Adam (Адам) и Eve (Ева).
+Унаследуй Еву от Адама.
+
+Требования:
+•	В классе Solution должен быть класс Adam (Адам).
+•	Класс Adam должен быть public.
+•	В классе Solution должен быть класс Eve (Ева).
+•	Класс Eve должен быть public.
+•	Класс Eve должен быть унаследован от класса Adam.*/
