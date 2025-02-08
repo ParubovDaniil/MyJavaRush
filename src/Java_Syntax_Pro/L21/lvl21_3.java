69598420fd6ca57dc337087eb62efbf83949ac59
ParubovDaniil <ParubovDaniil@mail.ru>
2025-02-08 10:09:03 +0500

    lvl21_3

---
+++ src/Java_Syntax_Pro/L21/lvl21_3.java
@@ -0,0 +1,32 @@
+package Java_Syntax_Pro.L21;
+
+public class lvl21_3 {}
+
+
+/*
+Лошадь и Пегас
+*/
+
+class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse{
+
+    }
+}
+/*
+*
+Отредактируй два класса — Horse (лошадь) и Pegasus (пегас).
+Унаследуй пегаса от лошади.
+
+Требования:
+•	В классе Solution должен быть класс Horse (лошадь).
+•	Класс Horse должен быть public.
+•	В классе Solution должен быть класс Pegasus (пегас).
+•	Класс Pegasus должен быть public.
+•	Класс Pegasus должен быть унаследован от класса Horse.*/
