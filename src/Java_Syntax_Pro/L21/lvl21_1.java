d519626d15f8750b72bb0f22b311533db689addc
ParubovDaniil <ParubovDaniil@mail.ru>
2025-02-08 10:08:01 +0500

    lvl21

---
+++ src/Java_Syntax_Pro/L21/lvl21_1.java
@@ -0,0 +1,23 @@
+package Java_Syntax_Pro.L21;
+
+public class lvl21_1 {}
+
+class Queen {
+    int x;
+    int y;
+    int worth;
+
+    void moveQueen() {
+
+    }
+}
+
+class Rook {
+    int x;
+    int y;
+    int worth;
+
+    void moveRook() {
+
+    }
+}
---
+++ src/Java_Syntax_Pro/L21/lvl21_2.java
@@ -0,0 +1,21 @@
+package Java_Syntax_Pro.L21;
+
+public class lvl21_2 {}
+
+class ChessItem {
+    int x;
+    int y;
+    int worth;
+}
+
+class Queen1 extends ChessItem {
+    void moveQueen() {
+
+    }
+}
+
+class Rook1 extends ChessItem {
+    void moveRook() {
+
+    }
+}
